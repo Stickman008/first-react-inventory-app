74f982c1d1bc24a8423c2d7672ac1e17a1259e56
Stickman008 <arm.supakorn@gmail.com>
2021-07-06 12:26:26 +0700

    Create and Setup react

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>HELLO WORLD THIS IS REACT!</p>
+    </div>
+  );
+}
+
+export default App;
