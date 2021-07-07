d4d31340d0d29971b4ee34221d953dcb0bd634d9
Stickman008 <arm.supakorn@gmail.com>
2021-07-07 12:14:54 +0700

    Full setup (Clear)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <p>HELLO WORLD THIS IS REACT!</p>
+      <Info/>
+      <AddItem/>
     </div>
   );
+}
+
+function Info() {
+  const title = "This is title";
+  const showTitle = true;
+
+  if(showTitle) {
+    return (
+      <div>
+        <h1>{title}</h1>
+        <p>Manage</p>
+      </div>
+    )
+  }
+
+  return <p>NOTHING</p>
+}
+
+function AddItem() {
+  return (
+    <form>
+      <lable for="text-form">Type something </lable>
+      <input type="text" id="text-form"></input>
+    </form>
+  )
 }
 
 export default App;
