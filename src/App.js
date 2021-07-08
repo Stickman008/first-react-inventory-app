255cfdb6ef358dac9f13814f36a2310f12d3c545
Stickman008 <arm.supakorn@gmail.com>
2021-07-08 13:53:06 +0700

    Clear for start project

--- src/App.js
+++ src/App.js
@@ -1,38 +1,13 @@
 import React, { Component }  from 'react';
 import './App.css';
-import Info from "./Info.js";
 import PropTypes from "prop-types";
 
 function App() {
   return (
     <div className="App">
-      <Info text="Inventory" />
-      <AddItem text="test" />
-      <AddItem text="test2" />
-      <AddItem />
+
     </div>
   );
 }
-
-function AddItem(props) {
-  const value = props.text;
-
-  return (
-    <form>
-      <lable for="text-form">Type something: </lable>
-      <input type="text" value={value} id="text-form"></input>
-    </form>
-  );
-}
-
-AddItem.defaultProps = {
-  number: 1,
-  text: "default text"
-};
-
-AddItem.propTypes = {
-  number: PropTypes.number,
-  text: PropTypes.string
-};
 
 export default App;
